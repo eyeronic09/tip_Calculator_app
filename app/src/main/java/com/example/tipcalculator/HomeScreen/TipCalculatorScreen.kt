ab33b040a299c7b77d35814cfbddae7ff41c34c8
aryan <guptaaryan92136@gmail.com>
2025-06-25 21:02:35 +0530

    Remove unused imports from TipCalculatorScreen.kt

--- app/src/main/java/com/example/tipcalculator/HomeScreen/TipCalculatorScreen.kt
+++ app/src/main/java/com/example/tipcalculator/HomeScreen/TipCalculatorScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -17,7 +16,6 @@ import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
-import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
