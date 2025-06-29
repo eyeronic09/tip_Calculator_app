dcf20dc49016d9b4f8e4923edba70cc688aa1b43
aryan <guptaaryan92136@gmail.com>
2025-06-29 13:28:28 +0530

    Add CustomTipDialog component and integrate into TipScreen for custom tip input

--- app/src/main/java/com/example/tipcalculator/HomeScreen/TipCalculatorScreen.kt
+++ app/src/main/java/com/example/tipcalculator/HomeScreen/TipCalculatorScreen.kt
@@ -1,31 +1,20 @@
 package com.example.tipcalculator.HomeScreen
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.rememberScrollableState
 import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Button
@@ -39,11 +28,12 @@ import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -52,29 +42,21 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import kotlinx.coroutines.flow.toSet
-import kotlin.random.Random
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TipScreen(viewmodel: TipViewmodel) {
     val billAmount by viewmodel.BillAmount.collectAsStateWithLifecycle()
-    val tipPercentage by viewmodel.TipPercentage.collectAsState()
-    val PerPerson by viewmodel.TotalPerPerson.collectAsState()
-    val split by viewmodel.spiltCounter.collectAsState()
+    val showCustomTipDialog = remember { mutableStateOf(false) }
+    val PerPerson by viewmodel.TotalPerPerson.collectAsStateWithLifecycle()
+    val split by viewmodel.spiltCounter.collectAsStateWithLifecycle()
     Scaffold(
         modifier = Modifier
             .systemBarsPadding(),
-        floatingActionButton = {
-            FloatingActionButton(onClick = {}) {
-
-            }
-        },
         content = { paddingValues ->
             Column(
                 modifier = Modifier
@@ -152,7 +134,6 @@ fun TipScreen(viewmodel: TipViewmodel) {
                                     onClick = { viewmodel.incrementSpilt() },
                                     modifier = Modifier.size(40.dp),
                                     colors = IconButtonDefaults.iconButtonColors(containerColor = MaterialTheme.colorScheme.secondaryContainer),
-
                                     ) {
                                     Icon(
                                         imageVector = Icons.Filled.KeyboardArrowUp,
@@ -217,7 +198,9 @@ fun TipScreen(viewmodel: TipViewmodel) {
 
                         }
                         Button(
-                            onClick = { /* ... */ },
+                            onClick = {
+                                showCustomTipDialog.value = true
+                            },
                             shape = RectangleShape
                         ) {
                             Text("custom tip")
@@ -230,6 +213,13 @@ fun TipScreen(viewmodel: TipViewmodel) {
         }
 
     )
+    if (showCustomTipDialog.value) {
+        CustomTipDialog(
+            openDialog = showCustomTipDialog.value,
+            onDismissRequest = { showCustomTipDialog.value = false },
+            onClick = { /* handle custom tip confirm here */ }
+        )
+    }
 }
 
 @Preview(showSystemUi = true)
